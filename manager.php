869ff63ec7bbd77d66e18885444f7eb80b741b4e
GiwanGunawan <giwangunawan@gmail.com>
2021-03-16 23:51:25 +0700

    ubah manager

--- manager.php
+++ manager.php
@@ -29,14 +29,13 @@ try {
 
     // Run the bot!
     $bot->run();
-
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
     // Log telegram errors
     Longman\TelegramBot\TelegramLog::error($e);
 
     // Uncomment this to output any errors (ONLY FOR DEVELOPMENT!)
-    // echo $e;
+    echo $e;
 } catch (Longman\TelegramBot\Exception\TelegramLogException $e) {
     // Uncomment this to output log initialisation errors (ONLY FOR DEVELOPMENT!)
-    // echo $e;
+    echo $e;
 }

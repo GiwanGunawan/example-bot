3adbba7a073c83f4c99875bdef108c86ccd277b0
GiwanGunawan <giwangunawan@gmail.com>
2021-03-17 17:30:46 +0700

    1730

--- config.php
+++ config.php
@@ -63,12 +63,12 @@ return [
     ],
 
     // Enter your MySQL database credentials
-    // 'mysql'        => [
-    //     'host'     => '127.0.0.1',
-    //     'user'     => 'motekari_root',
-    //     'password' => '2rMv$pdCkQ3P',
-    //     'database' => 'motekari_telegram_bot',
-    // ],
+    'mysql'        => [
+        'host'     => '127.0.0.1',
+        'user'     => 'motekari_root',
+        'password' => '2rMv$pdCkQ3P',
+        'database' => 'motekari_telegram_bot',
+    ],
 
     // 'mysql'            => [
     //     'host'         => '127.0.0.1',

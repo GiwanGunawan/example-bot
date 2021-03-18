df5062454324c1aadc236f90bad65d47719904ff
GiwanGunawan <giwangunawan@gmail.com>
2021-03-18 13:15:58 +0700

    1315

--- config.php
+++ config.php
@@ -44,7 +44,7 @@ return [
     'commands'     => [
         // Define all paths for your custom commands
         'paths'   => [
-            __DIR__ . '/forward',
+            __DIR__ . '/Commands',
         ],
         // Here you can set any command-specific parameters
         'configs' => [

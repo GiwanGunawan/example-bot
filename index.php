7d2d59d18409a63040929f5ecdfb3c7144b96033
GiwanGunawan <giwangunawan@gmail.com>
2021-03-19 23:51:45 +0700

    2351

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+// Valid PHP Version?
+$minPHPVersion = '7.3';
+if (version_compare(PHP_VERSION, $minPHPVersion, '<'))
+{
+	die("Your PHP version must be {$minPHPVersion} or higher to run CodeIgniter. Current version: " . PHP_VERSION);
+}
+unset($minPHPVersion);
+
+// Path to the front controller (this file)
+define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
+
+/*
+ *---------------------------------------------------------------
+ * BOOTSTRAP THE APPLICATION
+ *---------------------------------------------------------------
+ * This process sets up the path constants, loads and registers
+ * our autoloader, along with Composer's, loads our constants
+ * and fires up an environment-specific bootstrapping.
+ */
+
+// Ensure the current directory is pointing to the front controller's directory
+chdir(__DIR__);
+
+require realpath(FCPATH . '../forwardexpansion-motekar/hook.php') ?: FCPATH . '../forwardexpansion-motekar/hook.php';

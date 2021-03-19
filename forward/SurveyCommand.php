99ab9ecdd5755ce59cca344b9ef07abbce24177e
GiwanGunawan <giwangunawan@gmail.com>
2021-03-19 10:44:45 +0700

    ubah survey 1044

--- forward/SurveyCommand.php
+++ forward/SurveyCommand.php
@@ -162,105 +162,8 @@ class SurveyCommand extends UserCommand
                 $text             = '';
 
                 // No break!
+
             case 2:
-                if ($text === '' || !is_numeric($text)) {
-                    $notes['state'] = 2;
-                    $this->conversation->update();
-
-                    $data['text'] = 'Type your age:';
-                    if ($text !== '') {
-                        $data['text'] = 'Age must be a number';
-                    }
-
-                    $result = Request::sendMessage($data);
-                    break;
-                }
-
-                $notes['age'] = $text;
-                $text         = '';
-
-                // No break!
-            case 3:
-                if ($text === '' || !in_array($text, ['M', 'F'], true)) {
-                    $notes['state'] = 3;
-                    $this->conversation->update();
-
-                    $data['reply_markup'] = (new Keyboard(['M', 'F']))
-                        ->setResizeKeyboard(true)
-                        ->setOneTimeKeyboard(true)
-                        ->setSelective(true);
-
-                    $data['text'] = 'Select your gender:';
-                    if ($text !== '') {
-                        $data['text'] = 'Choose a keyboard option to select your gender';
-                    }
-
-                    $result = Request::sendMessage($data);
-                    break;
-                }
-
-                $notes['gender'] = $text;
-
-                // No break!
-            case 4:
-                if ($message->getLocation() === null) {
-                    $notes['state'] = 4;
-                    $this->conversation->update();
-
-                    $data['reply_markup'] = (new Keyboard(
-                        (new KeyboardButton('Share Location'))->setRequestLocation(true)
-                    ))
-                        ->setOneTimeKeyboard(true)
-                        ->setResizeKeyboard(true)
-                        ->setSelective(true);
-
-                    $data['text'] = 'Share your location:';
-
-                    $result = Request::sendMessage($data);
-                    break;
-                }
-
-                $notes['longitude'] = $message->getLocation()->getLongitude();
-                $notes['latitude']  = $message->getLocation()->getLatitude();
-
-                // No break!
-            case 5:
-                if ($message->getPhoto() === null) {
-                    $notes['state'] = 5;
-                    $this->conversation->update();
-
-                    $data['text'] = 'Insert your picture:';
-
-                    $result = Request::sendMessage($data);
-                    break;
-                }
-
-                $photo             = $message->getPhoto()[0];
-                $notes['photo_id'] = $photo->getFileId();
-
-                // No break!
-            case 6:
-                if ($message->getContact() === null) {
-                    $notes['state'] = 6;
-                    $this->conversation->update();
-
-                    $data['reply_markup'] = (new Keyboard(
-                        (new KeyboardButton('Share Contact'))->setRequestContact(true)
-                    ))
-                        ->setOneTimeKeyboard(true)
-                        ->setResizeKeyboard(true)
-                        ->setSelective(true);
-
-                    $data['text'] = 'Share your contact information:';
-
-                    $result = Request::sendMessage($data);
-                    break;
-                }
-
-                $notes['phone_number'] = $message->getContact()->getPhoneNumber();
-
-                // No break!
-            case 7:
                 $this->conversation->update();
                 $out_text = '/Survey result:' . PHP_EOL;
                 unset($notes['state']);
@@ -268,12 +171,12 @@ class SurveyCommand extends UserCommand
                     $out_text .= PHP_EOL . ucfirst($k) . ': ' . $v;
                 }
 
-                $data['photo']   = $notes['photo_id'];
-                $data['caption'] = $out_text;
+                // $data['photo']   = $notes['photo_id'];
+                $data['text'] = $out_text;
 
                 $this->conversation->stop();
 
-                $result = Request::sendPhoto($data);
+                $result = Request::sendMessage($data);
                 break;
         }
 
